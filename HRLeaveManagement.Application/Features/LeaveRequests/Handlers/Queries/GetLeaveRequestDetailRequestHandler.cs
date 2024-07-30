691f47f08846cc7a9f0885da104ceba8ed2b209a
Aziz798 <bouazizimohamedaziz76@gmail.com>
2024-07-30 16:46:35 +0100

    CQRS implimentation get requests

---
+++ HRLeaveManagement.Application/Features/LeaveRequests/Handlers/Queries/GetLeaveRequestDetailRequestHandler.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using HRLeaveManagement.Application.DTOs.LeaveRequest;
+using HRLeaveManagement.Application.Features.LeaveTypes.Requests.Queries;
+using HRLeaveManagement.Application.Persistence.Contracts;
+using MediatR;
+
+namespace HRLeaveManagement.Application.Features.LeaveRequests.Handlers.Queries;
+
+public class GetLeaveRequestDetailRequestHandler:IRequestHandler<GetLeaveTypeDetailRequest,LeaveRequestDto>
+{
+    private readonly ILeaveRequestRepository _leaveRequestRepository;
+    public readonly IMapper _mapper;
+    
+    public GetLeaveRequestDetailRequestHandler(ILeaveRequestRepository leaveRequestRepository, IMapper mapper)
+    {
+        _leaveRequestRepository = leaveRequestRepository;
+        _mapper = mapper;
+    }
+    public async Task<LeaveRequestDto> Handle(GetLeaveTypeDetailRequest request, CancellationToken cancellationToken)
+    {
+        var leaveRequest = await _leaveRequestRepository.GetAsync(request.Id);
+        return _mapper.Map<LeaveRequestDto>(leaveRequest);
+    }
+}
--- HRLeaveManagement.Application/Features/LeaveTypes/Requests/Queries/GetLeaveTypeDetailRequest.cs
+++ HRLeaveManagement.Application/Features/LeaveTypes/Requests/Queries/GetLeaveTypeDetailRequest.cs
@@ -1,9 +1,10 @@
-﻿using HRLeaveManagement.Application.DTOs.LeaveType;
+﻿using HRLeaveManagement.Application.DTOs.LeaveRequest;
+using HRLeaveManagement.Application.DTOs.LeaveType;
 using MediatR;
 
 namespace HRLeaveManagement.Application.Features.LeaveTypes.Requests.Queries;
 
-public class GetLeaveTypeDetailRequest:IRequest<LeaveTypeDto>
+public class GetLeaveTypeDetailRequest:IRequest<LeaveTypeDto>, IRequest<LeaveRequestDto>
 {
     public int Id { get; set; }
 }
